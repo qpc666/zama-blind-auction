d77e8da87fbf649751c10a35c76aeab40d02e28e
钱鹏程 <qianpengcheng@qianpengchengdeMac-mini.local>
2025-12-08 14:58:52 +0800

    Fix: Add Node polyfills for Zama SDK

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "/confidential-blind-auction/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills()
+  ],
+  build: {
+    target: "esnext"
+  }
 })
